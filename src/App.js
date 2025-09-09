19717075f21858e4642e466d5c113d23b5fe7437
YogeshMaha <75209265+YogeshMaha@users.noreply.github.com>
2025-09-09 15:24:25 +0530

    Update App.js
    Test Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const test1 = () => {
+  console.log("launch the application");
+}
   return (
     <div className="App">
       <header className="App-header">
